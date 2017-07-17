531ddf0f70a2f939cd04427647b95b341fea1cbe
JerryOrange <563146556@qq.com>
2017-07-17 22:39:08 +0800

    增加mapper 接口类扫描包配置

--- spring-boot-restful/src/main/java/cn/ncu/jerry/SpringBootRestfulApplication.java
+++ spring-boot-restful/src/main/java/cn/ncu/jerry/SpringBootRestfulApplication.java
@@ -1,9 +1,11 @@
 package cn.ncu.jerry;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.ncu.jerry.dao")
 public class SpringBootRestfulApplication {
 
 	public static void main(String[] args) {
