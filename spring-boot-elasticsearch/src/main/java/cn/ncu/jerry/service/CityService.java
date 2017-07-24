89f2cfa264119dd3d5f4d52c5ffbe9f57a67bf52
JerryOrange <563146556@qq.com>
2017-07-24 17:50:42 +0800

    新增service接口类

---
+++ spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/service/CityService.java
@@ -0,0 +1,28 @@
+package cn.ncu.jerry.service;
+
+import cn.ncu.jerry.entity.City;
+import java.util.List;
+
+/**
+ * Created by Jiacheng on 2017/7/24.
+ */
+public interface CityService {
+
+    /**
+     * 新增城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long saveCity(City city);
+
+    /**
+     * 根据关键词，function score query 权重分分页查询
+     *
+     * @param pageNumber
+     * @param pageSize
+     * @param searchContent
+     * @return
+     */
+    List<City> searchCity(Integer pageNumber, Integer pageSize, String searchContent);
+}
