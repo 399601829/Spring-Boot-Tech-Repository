4ed6742a52b937f992db726b6dbc14a77d27a55e
JerryOrange <563146556@qq.com>
2017-07-27 10:28:24 +0800

    城市Controller实现Restful HTTP服务

---
+++ spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/controller/CityRestController.java
@@ -0,0 +1,32 @@
+package cn.ncu.jerry.controller;
+
+import cn.ncu.jerry.entity.City;
+import cn.ncu.jerry.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 城市 Controller 实现 Restful HTTP 服务
+ *
+ * Created by Jiacheng on 2017/7/25.
+ */
+@RestController
+public class CityRestController {
+
+    @Autowired
+    private CityService cityService;
+
+    @RequestMapping(value = "/api/city", method = RequestMethod.POST)
+    public Long createCity(City city) {
+        return cityService.saveCity(city);
+    }
+
+    @RequestMapping(value = "/api/city/search", method = RequestMethod.GET)
+    public List<City> searchCity(@RequestParam(value = "pageNumber") Integer pageNumber,
+                                 @RequestParam(value = "pageSize", required = false) Integer pageSize,
+                                 @RequestParam(value = "searchContent") String searchContent) {
+        return cityService.searchCity(pageNumber,pageSize,searchContent);
+    }
+}
