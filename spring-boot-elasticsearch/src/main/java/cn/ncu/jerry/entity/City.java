571ce98757d9c24c0bf2422b8d237caca70914f1
JerryOrange <563146556@qq.com>
2017-07-29 10:13:28 +0800

    添加注释

--- spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/entity/City.java
+++ spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/entity/City.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
  * Created by Jiacheng on 2017/7/24.
  */
 @Document(indexName = "cityindex", type = "city")
+//index 配置必须是全部小写，不然会报异常：org.elasticsearch.indices.InvalidIndexNameException: Invalid index name [cityIndex], must be lowercase
 public class City implements Serializable {
 
     private static final long serialVersionUID = -1L;
