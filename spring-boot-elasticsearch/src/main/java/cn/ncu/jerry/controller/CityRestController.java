ddc7b40c0677193f251808a7a7dc778081e8a83d
JerryOrange <563146556@qq.com>
2017-08-01 15:29:53 +0800

    新增etIkAnalyzeSearchTerms方法调用 ES 获取 IK 分词后结果

--- spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/controller/CityRestController.java
+++ spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/controller/CityRestController.java
@@ -2,9 +2,14 @@ package cn.ncu.jerry.controller;
 
 import cn.ncu.jerry.entity.City;
 import cn.ncu.jerry.service.CityService;
+import org.elasticsearch.action.admin.indices.analyze.AnalyzeAction;
+import org.elasticsearch.action.admin.indices.analyze.AnalyzeRequestBuilder;
+import org.elasticsearch.action.admin.indices.analyze.AnalyzeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +22,8 @@ public class CityRestController {
 
     @Autowired
     private CityService cityService;
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @RequestMapping(value = "/api/city", method = RequestMethod.POST)
     public String createCity(@RequestBody City city) {
@@ -28,5 +35,25 @@ public class CityRestController {
                                  @RequestParam(value = "pageSize", required = false) Integer pageSize,
                                  @RequestParam(value = "searchContent") String searchContent) {
         return cityService.searchCity(pageNumber,pageSize,searchContent);
+    }
+
+    /**
+     * 调用 ES 获取 IK 分词后结果
+     *
+     * @param searchContent
+     * @return
+     */
+    @RequestMapping(value = "ikSearch", method = RequestMethod.GET)
+    private List<String> getIkAnalyzeSearchTerms(String searchContent) {
+        // 调用 IK 分词分词
+        AnalyzeRequestBuilder ikRequest = new AnalyzeRequestBuilder(elasticsearchTemplate.getClient(), AnalyzeAction.INSTANCE,"cityindex",searchContent);
+        ikRequest.setTokenizer("ik");
+        List<AnalyzeResponse.AnalyzeToken> ikTokenList = ikRequest.execute().actionGet().getTokens();
+
+        // 循环赋值
+        List<String> searchTermList = new ArrayList<>();
+        ikTokenList.forEach(ikToken -> { searchTermList.add(ikToken.getTerm()); });
+
+        return searchTermList;
     }
 }
