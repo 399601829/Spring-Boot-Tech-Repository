faff92b8f85d621faaac4e92f6866bb8b0d6af49
JerryOrange <563146556@qq.com>
2017-08-04 15:37:20 +0800

    add servlet to handle common http error

--- spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/SpringBootElasticsearchApplication.java
+++ spring-boot-elasticsearch/src/main/java/cn/ncu/jerry/SpringBootElasticsearchApplication.java
@@ -2,6 +2,10 @@ package cn.ncu.jerry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * Spring Boot 应用启动类
@@ -17,5 +21,17 @@ public class SpringBootElasticsearchApplication {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(SpringBootElasticsearchApplication.class,args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return (container -> {
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+            container.addErrorPages(error401Page, error404Page, error500Page);
+        });
     }
 }
